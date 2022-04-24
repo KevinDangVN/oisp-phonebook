6219ee77e1a3a25bfa490b9d7959e934ea6ed138
kevin <dqtkien@gmail.com>
2022-04-24 11:35:33 +0700

    Add some excel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { readFile, utils } from 'xlsx'
+import path from 'path'
 import Footer from '../components/Footer'
 import Heading from '../components/Heading/Heading'
 import Layout from '../components/Layout/Layout'
 import Logo from '../components/Logo/Logo'
 import Table from '../components/Table/Table'
+import { IPhoneBookData } from '../interface/IPhoneBook'
 
 const Home: NextPage = () => {
   return (
@@ -21,3 +24,32 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps<{
+  data: IPhoneBookData[]
+  success: boolean
+}> = async () => {
+  try {
+    const filePath = path.join(__dirname, '..')
+    console.log(filePath)
+    // const workbook = readFile('../assets/data_dien_thoai.xlsx')
+    // const sheet = workbook.Sheets[workbook.SheetNames[0]]
+    // const jsonSheet = utils.sheet_to_json(sheet)
+    // console.log(jsonSheet)
+
+    return {
+      props: {
+        success: true,
+        data: [{ extension: 12, name: '', team: '' }],
+      },
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      props: {
+        success: false,
+        data: [],
+      },
+    }
+  }
+}
