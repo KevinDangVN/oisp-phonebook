f87b22cbd172345e91a9f43f2df0d04965ec8ff6
kevin <dqtkien@gmail.com>
2022-04-24 09:35:34 +0700

    Add layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
 
 const Layout: FC = ({ children }) => (
-  <div className="bg-slate-50 h-screen w-full flex justify-center align-middle items-center px-3 flex-col">
-    <main>{children}</main>
+  <div className="bg-slate-50 h-screen w-full flex justify-center align-middle items-center px-3 py-3 flex-col">
+    {children}
   </div>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,12 @@ import Table from '../components/Table/Table'
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Table />
-      <Footer />
+      <main className="w-11/12 max-w-[1000px] bg-white lg:w-[9000px] sm:w-[600px] md:w-[700px]  overflow-y-auto h-[85vh] rounded drop-shadow px-3 py-3">
+        <Table />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </Layout>
   )
 }
