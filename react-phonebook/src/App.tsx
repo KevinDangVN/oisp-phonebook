409699d7797ad6587cd96cc45b9dabe22c6a9fa3
kevin <dqtkien@gmail.com>
2022-04-25 19:02:26 +0700

    Still lack of getData

--- react-phonebook/src/App.tsx
+++ react-phonebook/src/App.tsx
@@ -4,8 +4,21 @@ import Logo from './components/Logo/Logo'
 import Heading from './components/Heading/Heading'
 import Table from './components/Table/Table'
 import Footer from './components/Footer/Footer'
+import readXlsxFile from 'read-excel-file'
+import { useEffect } from 'react'
+import { readFile, utils } from 'xlsx'
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const workbook = readFile('/data.xlsx')
+      const sheet = workbook.Sheets[workbook.SheetNames[0]]
+      const jsonSheet = utils.sheet_to_json(sheet)
+      console.log(jsonSheet)
+    }
+    getData()
+  }, [])
+
   return (
     <Layout>
       <main className="w-11/12 max-w-[1000px] bg-white lg:w-[9000px] sm:w-[600px] md:w-[700px] h-full rounded drop-shadow px-3 py-3 mb-3 prose prose-gray flex-col relative">
