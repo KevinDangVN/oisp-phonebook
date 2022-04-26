10d356086976e4ce886ba9356a36a85c4873dcbc
kevin <dqtkien@gmail.com>
2022-04-26 23:56:41 +0700

    Add search text

--- react-phonebook/src/components/Search/Search.tsx
+++ react-phonebook/src/components/Search/Search.tsx
@@ -8,18 +8,18 @@ interface ISearchProps {
 export const Search: FC<ISearchProps> = ({ text, onChange }) => {
   return (
     <form>
-      <div className="grid gap-6 mb-6 lg:grid-cols-2 px-5">
-        <div>
+      <div className="flex mb-6 px-5 text-center justify-center">
+        <div className="justify-start">
           <label
             htmlFor="search"
-            className="block mb-2 text-sm font-medium text-gray-900"
+            className="block mb-2 text-sm font-medium text-gray-900 text-left"
           >
             Search
           </label>
           <input
             type="text"
             id="search"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus-visible:border-blue-500 block w-full p-2.5 focus:outline-none"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus-visible:border-blue-500 block w-[600px] p-2.5 focus:outline-none"
             placeholder=""
             value={text || ''}
             onChange={onChange}
